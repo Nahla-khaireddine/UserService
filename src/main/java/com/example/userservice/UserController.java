223f546fe0979a67ba011de6c976f1b0b186c42e
Nahla Khaireddine <nahlak@nahlak-a01.vmware.com>
2022-03-14 15:15:08 -0700

    initial commit

---
+++ src/main/java/com/example/userservice/UserController.java
@@ -0,0 +1,13 @@
+package com.example.userservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping
+    public String getUsers() {
+        return "user is nahla kh";
+    }
+}
